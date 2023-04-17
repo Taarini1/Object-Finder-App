fa53609c0b5f366c17a63081cc1635224ac3dc84
Taarini1 <98458225+Taarini1@users.noreply.github.com>
2023-04-17 17:44:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
-
-
+img="";
+Status=""
 function preload(){
+
+
+
 
 }
 
 function setup(){
-canvas=createCanvas(450,200)
+canvas=createCanvas(450,200);
 canvas.position(460,450);
+objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML = "Objects are being detected";
+}
+
+
+function modelLoaded(){
+
+objectDetector.detect(img,gotResults);
+console.log("Model Loaded");
+Status=true;
+}
+
+function gotResults(error,results){
+if(error){console.log(error)};
+if(results){
+    console.log(results);
+
+
+
+
+}
 
 
 }
 
 function draw(){
-
+    image(img,0,0,750,100);
 }
